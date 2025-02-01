9b0bf213546d279fe3ffec49c62fe8df3e3169c1
Sebastian Molle <sebastian.molle@gmail.com>
2025-02-01 07:43:01 +0100

    Refactoring

--- bin/container-entrypoint.d/entrypoint.d/10-folders.sh
+++ bin/container-entrypoint.d/entrypoint.d/10-folders.sh
@@ -2,10 +2,10 @@
 
 export APP_BIN_DIR="/app/sbin"
 export APP_SRC_DIR="/app/src/elasticms"
-export APP_CONFIG_DIR="/app/tmp/elasticms.d"
+export APP_TMP_DIR="/app/tmp"
 
-export APP_ASSETS_DIR="${APP_SRC_DIR}/public"
-#export APP_ASSETS_DIR="/app/var/assets/elasticms"
+export APP_CONFIG_DIR="${APP_TMP_DIR}/elasticms.d"
 export APP_CACHE_DIR="/app/var/cache/elasticms"
+export APP_LOG_DIR="/app/var/log/elasticms"
 
 true
