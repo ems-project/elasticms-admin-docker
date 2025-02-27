ea507ef7d40f0a42ed5868525a418ae7b00024b4
Sébastian Molle <sebastian.molle@gmail.com>
2025-02-27 12:49:25 +0100

    Fix missing file extension for supervisor generated config at startup

--- bin/container-entrypoint.d/elasticms.d/30-jobs.sh
+++ bin/container-entrypoint.d/elasticms.d/30-jobs.sh
@@ -7,7 +7,7 @@ if [[ ! -z ${JOBS_ENABLED} ]] && [[ ${JOBS_ENABLED,,} = true ]]; then
   log "INFO" "+ Use Supervisor for running ElasticMS Admin Jobs ..."
 
   gomplate -f /app/config/supervisor/eventlistener.ini.gtpl \
-           -o /app/etc/supervisor.d/${ELASTICMS_INSTANCE_NAME}
+           -o /app/etc/supervisor.d/${ELASTICMS_INSTANCE_NAME}.ini
 
   gomplate -f /app/config/sbin/ems-job-run.sh.gtpl \
            -o ${APP_BIN_DIR}/ems-jobs/${ELASTICMS_INSTANCE_NAME}
