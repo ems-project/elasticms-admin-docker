1d842c0d2b87927b433e5cd73396fc08655499b5
Sébastian Molle <sebastian.molle@gmail.com>
2025-05-28 12:56:40 +0200

    Fix: metrics startup script not use correct variable name (#128)

--- bin/container-entrypoint.d/elasticms.d/80-metrics.sh
+++ bin/container-entrypoint.d/elasticms.d/80-metrics.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ ! -z ${EMS_METRICS_ENABLED} ]] && [[ ${EMS_METRICS_ENABLED,,} = true ]]; then
+if [[ ! -z ${EMS_METRIC_ENABLED} ]] && [[ ${EMS_METRIC_ENABLED,,} = true ]]; then
 
   log "INFO" "+ Clear Elasticms metrics for [ ${ELASTICMS_INSTANCE_NAME} ] CMS Domain ..."
 
   ${APP_BIN_DIR}/${ELASTICMS_INSTANCE_NAME} ems:metric:collect --clear --env=${APP_ENV}
 
-  if [ $? -eq 0 ]; then
-    log "ERROR" "! Something doesn't work with Elasticms metrics clearing !"
+  if [ $? -ne 0 ]; then
+    log "WARN" "! Something doesn't work with Elasticms metrics clearing !"
   fi
 
 fi
